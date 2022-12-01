89979394e71dd12ec079def6cfd0e755351812d0
whoismarie2 <114930980+whoismarie2@users.noreply.github.com>
2022-12-01 18:04:31 -0500

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,6 @@
+//Este codigo hace que los puntitos no se repitan
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector('.contenedor_loader')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+})
